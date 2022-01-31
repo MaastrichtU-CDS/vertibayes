bf1ce7401e97274305d3578576bb224cd41f0eae
Florian <florian.vandaalen@maastro.nl>
2022-01-31 16:14:45 +0100

    Cleaned up unittest

--- src/test/java/com/florian/vertibayes/bayes/webservice/VertiBayesCentralServerTest.java
+++ src/test/java/com/florian/vertibayes/bayes/webservice/VertiBayesCentralServerTest.java
@@ -1,5 +1,7 @@
 package com.florian.vertibayes.bayes.webservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.florian.nscalarproduct.webservice.ServerEndpoint;
 import com.florian.vertibayes.bayes.Node;
 import com.florian.vertibayes.bayes.ParentValue;
@@ -18,6 +20,44 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VertiBayesCentralServerTest {
+
+    @Test
+    public void testBinning() throws JsonProcessingException {
+        BayesServer station1 = new BayesServer("resources/Experiments/iris/iris_firsthalf.csv", "1");
+        BayesServer station2 = new BayesServer("resources/Experiments/iris/iris_secondhalf.csv", "2");
+
+        VertiBayesEndpoint endpoint1 = new VertiBayesEndpoint(station1);
+        VertiBayesEndpoint endpoint2 = new VertiBayesEndpoint(station2);
+        BayesServer secret = new BayesServer("3", Arrays.asList(endpoint1, endpoint2));
+
+        ServerEndpoint secretEnd = new ServerEndpoint(secret);
+
+        List<ServerEndpoint> all = new ArrayList<>();
+        all.add(endpoint1);
+        all.add(endpoint2);
+        all.add(secretEnd);
+        secret.setEndpoints(all);
+        station1.setEndpoints(all);
+        station2.setEndpoints(all);
+
+        VertiBayesCentralServer central = new VertiBayesCentralServer();
+        central.initEndpoints(Arrays.asList(endpoint1, endpoint2), secretEnd);
+        List<WebNode> noBinsWeb = central.buildNetwork().getNodes();
+        WebBayesNetwork req = new WebBayesNetwork();
+        req.setNodes(noBinsWeb);
+
+        List<Node> noBins = WebNodeMapper.mapWebNodeToNode(central.maximumLikelyhood(req).getNodes());
+
+        List<WebNode> bins = central.buildNetwork().getNodes();
+        WebBayesNetwork reqBins = new WebBayesNetwork();
+
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(
+                mapper.writerWithDefaultPrettyPrinter().writeValueAsString(central.maximumLikelyhood(req).getNodes()));
+
+        reqBins.setNodes(bins);
+
+    }
 
     @Test
     public void testMaximumLikelyhood() {
@@ -58,17 +98,23 @@ public class VertiBayesCentralServerTest {
         // now check the thetas
         // node 1 has 2 values and no parents, reuslting in 2 values in total
         assertEquals(nodes.get(0).getProbabilities().size(), 2);
+        // put them in a map to easily find correct probability:
+        HashMap<String, Theta> map = new HashMap<>();
+        for (Theta t : nodes.get(0).getProbabilities()) {
+            map.put(t.getLocalValue().getValue(), t);
+        }
+
         // value 1
-        assertEquals(nodes.get(0).getProbabilities().get(0).getP(), 0.5);
-        assertEquals(nodes.get(0).getProbabilities().get(0).getLocalValue().getValue(), "0");
-        assertEquals(nodes.get(0).getProbabilities().get(0).getLocalValue().getAttributeName(), "x1");
-        assertEquals(nodes.get(0).getProbabilities().get(0).getParents().size(), 0);
+        assertEquals(map.get("0").getP(), 0.5);
+        assertEquals(map.get("0").getLocalValue().getValue(), "0");
+        assertEquals(map.get("0").getLocalValue().getAttributeName(), "x1");
+        assertEquals(map.get("0").getParents().size(), 0);
 
         // value 2
-        assertEquals(nodes.get(0).getProbabilities().get(1).getP(), 0.5);
-        assertEquals(nodes.get(0).getProbabilities().get(1).getLocalValue().getValue(), "1");
-        assertEquals(nodes.get(0).getProbabilities().get(1).getLocalValue().getAttributeName(), "x1");
-        assertEquals(nodes.get(0).getProbabilities().get(1).getParents().size(), 0);
+        assertEquals(map.get("1").getP(), 0.5);
+        assertEquals(map.get("1").getLocalValue().getValue(), "1");
+        assertEquals(map.get("1").getLocalValue().getAttributeName(), "x1");
+        assertEquals(map.get("1").getParents().size(), 0);
 
         //assert all probabilities are viewed as sliblings as there are no parents
         List<Theta> sliblings = Node.findSliblings(nodes.get(0).getProbabilities().get(0), nodes.get(0));
@@ -78,39 +124,41 @@ public class VertiBayesCentralServerTest {
         //node 2 has 2 local values and 1 parent with 2 values resulting in 4 values in total
         // value 1
         assertEquals(nodes.get(1).getProbabilities().size(), 4);
-        assertEquals(nodes.get(1).getProbabilities().get(0).getP(), 0.8);
-        assertEquals(nodes.get(1).getProbabilities().get(0).getLocalValue().getValue(), "0");
-        assertEquals(nodes.get(1).getProbabilities().get(0).getLocalValue().getAttributeName(), "x2");
-        assertEquals(nodes.get(1).getProbabilities().get(0).getParents().size(), 1);
-        assertEquals(nodes.get(1).getProbabilities().get(0).getParents().get(0).getValue().getValue(), "0");
-        assertEquals(nodes.get(1).getProbabilities().get(0).getParents().get(0).getName(), "x1");
+        // put them in a map to easily find correct probability:
+        map = new HashMap<>();
+        for (Theta t : nodes.get(1).getProbabilities()) {
+            String key = t.getLocalValue().getValue() + t.getParents().get(0).getValue().getValue();
+            map.put(key, t);
+        }
 
-        // value 2
-        assertEquals(nodes.get(1).getProbabilities().size(), 4);
-        assertEquals(nodes.get(1).getProbabilities().get(1).getP(), 0.2);
-        assertEquals(nodes.get(1).getProbabilities().get(1).getLocalValue().getValue(), "1");
-        assertEquals(nodes.get(1).getProbabilities().get(1).getLocalValue().getAttributeName(), "x2");
-        assertEquals(nodes.get(1).getProbabilities().get(1).getParents().size(), 1);
-        assertEquals(nodes.get(1).getProbabilities().get(1).getParents().get(0).getValue().getValue(), "0");
-        assertEquals(nodes.get(1).getProbabilities().get(1).getParents().get(0).getName(), "x1");
+        assertEquals(map.get("00").getP(), 0.8);
+        assertEquals(map.get("00").getLocalValue().getValue(), "0");
+        assertEquals(map.get("00").getLocalValue().getAttributeName(), "x2");
+        assertEquals(map.get("00").getParents().size(), 1);
+        assertEquals(map.get("00").getParents().get(0).getValue().getValue(), "0");
+        assertEquals(map.get("00").getParents().get(0).getName(), "x1");
 
-        // value 3
-        assertEquals(nodes.get(1).getProbabilities().size(), 4);
-        assertEquals(nodes.get(1).getProbabilities().get(2).getP(), 0.2);
-        assertEquals(nodes.get(1).getProbabilities().get(2).getLocalValue().getValue(), "0");
-        assertEquals(nodes.get(1).getProbabilities().get(2).getLocalValue().getAttributeName(), "x2");
-        assertEquals(nodes.get(1).getProbabilities().get(2).getParents().size(), 1);
-        assertEquals(nodes.get(1).getProbabilities().get(2).getParents().get(0).getValue().getValue(), "1");
-        assertEquals(nodes.get(1).getProbabilities().get(2).getParents().get(0).getName(), "x1");
+        assertEquals(map.get("10").getP(), 0.2);
+        assertEquals(map.get("10").getLocalValue().getValue(), "1");
+        assertEquals(map.get("10").getLocalValue().getAttributeName(), "x2");
+        assertEquals(map.get("10").getParents().size(), 1);
+        assertEquals(map.get("10").getParents().get(0).getValue().getValue(), "0");
+        assertEquals(map.get("10").getParents().get(0).getName(), "x1");
 
-        // value 4
-        assertEquals(nodes.get(1).getProbabilities().size(), 4);
-        assertEquals(nodes.get(1).getProbabilities().get(3).getP(), 0.8);
-        assertEquals(nodes.get(1).getProbabilities().get(3).getLocalValue().getValue(), "1");
-        assertEquals(nodes.get(1).getProbabilities().get(3).getLocalValue().getAttributeName(), "x2");
-        assertEquals(nodes.get(1).getProbabilities().get(3).getParents().size(), 1);
-        assertEquals(nodes.get(1).getProbabilities().get(3).getParents().get(0).getValue().getValue(), "1");
-        assertEquals(nodes.get(1).getProbabilities().get(3).getParents().get(0).getName(), "x1");
+        assertEquals(map.get("01").getP(), 0.2);
+        assertEquals(map.get("01").getLocalValue().getValue(), "0");
+        assertEquals(map.get("01").getLocalValue().getAttributeName(), "x2");
+        assertEquals(map.get("01").getParents().size(), 1);
+        assertEquals(map.get("01").getParents().get(0).getValue().getValue(), "1");
+        assertEquals(map.get("01").getParents().get(0).getName(), "x1");
+
+        assertEquals(map.get("11").getP(), 0.8);
+        assertEquals(map.get("11").getLocalValue().getValue(), "1");
+        assertEquals(map.get("11").getLocalValue().getAttributeName(), "x2");
+        assertEquals(map.get("11").getParents().size(), 1);
+        assertEquals(map.get("11").getParents().get(0).getValue().getValue(), "1");
+        assertEquals(map.get("11").getParents().get(0).getName(), "x1");
+
 
         //assert there are two sets of sliblings as there is 1 parent with 2 unique values
         //Set 1=2 and set 3=4 are supposed to be equal due to the starting node used
@@ -127,43 +175,39 @@ public class VertiBayesCentralServerTest {
         assertTrue(sliblings1.containsAll(nodes.get(1).getProbabilities()));
 
         //node 3 has 2 local values and 1 parent with 2 values resulting in 4 values in total
-        // value 1
-        assertEquals(nodes.get(2).getProbabilities().size(), 4);
-        assertEquals(nodes.get(2).getProbabilities().get(0).getP(), 0.8);
-        assertEquals(nodes.get(2).getProbabilities().get(0).getLocalValue().getValue(), "0");
-        assertEquals(nodes.get(2).getProbabilities().get(0).getLocalValue().getAttributeName(), "x3");
-        assertEquals(nodes.get(2).getProbabilities().get(0).getParents().size(), 1);
-        assertEquals(nodes.get(2).getProbabilities().get(0).getParents().get(0).getValue().getValue(), "0");
-        assertEquals(nodes.get(2).getProbabilities().get(0).getParents().get(0).getName(), "x2");
+        // put them in a map to easily find correct probability:
+        map = new HashMap<>();
+        for (Theta t : nodes.get(2).getProbabilities()) {
+            String key = t.getLocalValue().getValue() + t.getParents().get(0).getValue().getValue();
+            map.put(key, t);
+        }
+        assertEquals(map.get("00").getP(), 0.8);
+        assertEquals(map.get("00").getLocalValue().getValue(), "0");
+        assertEquals(map.get("00").getLocalValue().getAttributeName(), "x3");
+        assertEquals(map.get("00").getParents().size(), 1);
+        assertEquals(map.get("00").getParents().get(0).getValue().getValue(), "0");
+        assertEquals(map.get("00").getParents().get(0).getName(), "x2");
 
-        // value 2
-        assertEquals(nodes.get(2).getProbabilities().size(), 4);
-        assertEquals(nodes.get(2).getProbabilities().get(1).getP(), 0.2);
-        assertEquals(nodes.get(2).getProbabilities().get(1).getLocalValue().getValue(), "1");
-        assertEquals(nodes.get(2).getProbabilities().get(1).getLocalValue().getAttributeName(), "x3");
-        assertEquals(nodes.get(2).getProbabilities().get(1).getParents().size(), 1);
-        assertEquals(nodes.get(2).getProbabilities().get(1).getParents().get(0).getValue().getValue(), "0");
-        assertEquals(nodes.get(2).getProbabilities().get(1).getParents().get(0).getName(), "x2");
+        assertEquals(map.get("10").getP(), 0.2);
+        assertEquals(map.get("10").getLocalValue().getValue(), "1");
+        assertEquals(map.get("10").getLocalValue().getAttributeName(), "x3");
+        assertEquals(map.get("10").getParents().size(), 1);
+        assertEquals(map.get("10").getParents().get(0).getValue().getValue(), "0");
+        assertEquals(map.get("10").getParents().get(0).getName(), "x2");
 
-        // value 3
-        assertEquals(nodes.get(2).getProbabilities().size(), 4);
-        // test data would expect 0.0 here, but minimum likelyhood forces it to 0.001
-        assertEquals(nodes.get(2).getProbabilities().get(2).getP(), 0.001);
-        assertEquals(nodes.get(2).getProbabilities().get(2).getLocalValue().getValue(), "0");
-        assertEquals(nodes.get(2).getProbabilities().get(2).getLocalValue().getAttributeName(), "x3");
-        assertEquals(nodes.get(2).getProbabilities().get(2).getParents().size(), 1);
-        assertEquals(nodes.get(2).getProbabilities().get(2).getParents().get(0).getValue().getValue(), "1");
-        assertEquals(nodes.get(2).getProbabilities().get(2).getParents().get(0).getName(), "x2");
+        assertEquals(map.get("01").getP(), 0.001);
+        assertEquals(map.get("01").getLocalValue().getValue(), "0");
+        assertEquals(map.get("01").getLocalValue().getAttributeName(), "x3");
+        assertEquals(map.get("01").getParents().size(), 1);
+        assertEquals(map.get("01").getParents().get(0).getValue().getValue(), "1");
+        assertEquals(map.get("01").getParents().get(0).getName(), "x2");
 
-        // value 4
-        assertEquals(nodes.get(2).getProbabilities().size(), 4);
-        // test data would expect 1 here, but minimum likelyhood of its slibling forces it to 0.999
-        assertEquals(nodes.get(2).getProbabilities().get(3).getP(), 0.999);
-        assertEquals(nodes.get(2).getProbabilities().get(3).getLocalValue().getValue(), "1");
-        assertEquals(nodes.get(2).getProbabilities().get(3).getLocalValue().getAttributeName(), "x3");
-        assertEquals(nodes.get(2).getProbabilities().get(3).getParents().size(), 1);
-        assertEquals(nodes.get(2).getProbabilities().get(3).getParents().get(0).getValue().getValue(), "1");
-        assertEquals(nodes.get(2).getProbabilities().get(3).getParents().get(0).getName(), "x2");
+        assertEquals(map.get("11").getP(), 0.999);
+        assertEquals(map.get("11").getLocalValue().getValue(), "1");
+        assertEquals(map.get("11").getLocalValue().getAttributeName(), "x3");
+        assertEquals(map.get("11").getParents().size(), 1);
+        assertEquals(map.get("11").getParents().get(0).getValue().getValue(), "1");
+        assertEquals(map.get("11").getParents().get(0).getName(), "x2");
 
         //assert there are two sets of sliblings as there is 1 parent with 2 unique values
         //Set 1=2 and set 3=4 are supposed to be equal due to the starting node used
